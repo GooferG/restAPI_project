3631162953882a654e572635c22ccfc5da988a26
Luiz M <luiz.meneghim@hotmail.com>
2022-05-26 22:19:30 -0700

    making prisma run and setting up project

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
   if (req.method !== 'GET') {
     res.status(405).json({
@@ -7,5 +5,5 @@ export default function handler(req, res) {
     });
     return;
   }
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ test: 'TRIPS!' });
 }
